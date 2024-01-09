3aa52a9a6b94e93dec6938fa29a9b1c1c48e1f87
0xN1ghtStalker <nixcution@gmail.com>
2024-01-09 11:41:42 -0500

    Update README.md

---
+++ my-code.py
@@ -0,0 +1,9 @@
+def main() :
+    name = "professor nedd"
+    print("We want to know if you like programming")
+    answer = input("Do you like programming "+name+ "?")
+    print('Great! You said "'+answer+'!"')
+    print("Let's learn some python today")
+
+    if __name__ == "__main__":
+        main()
